04d02551f2167e64d451415da7ad3c065869dfce
Santiago <santiagobalino@hotmail.com>
2022-11-25 03:06:07 -0300

    Add slides and styles for the carrousel and a smooth transition

--- src/components/views/Main/Main.tsx
+++ src/components/views/Main/Main.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import {
     Box,
     Button,
     Heading,
+    Icon,
     Menu,
     MenuButton,
     MenuItem,
     MenuList,
     Stack,
 } from '@chakra-ui/react';
-import { ChevronDownIcon } from '@chakra-ui/icons';
+import {
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    ChevronDownIcon,
+} from '@chakra-ui/icons';
 import {
     useGetMoviesByFilterQuery,
     useGetMoviesByTrendQuery,
@@ -48,6 +53,36 @@ export const Main = () => {
         trendingFilter.toLowerCase()
     );
     const moviesByTrend = movies?.results;
+
+    const popularCarrousel = useRef<HTMLDivElement>(null);
+    const trendCarrousel = useRef<HTMLDivElement>(null);
+
+    //TODO: Refactor these functions into one that accepts 2 parameters.
+
+    const handleSlideOne = (position: 'left' | 'right') => {
+        if (popularCarrousel.current !== null) {
+            if (position === 'left') {
+                popularCarrousel.current.scrollLeft =
+                    popularCarrousel.current.scrollLeft - 500;
+            }
+            if (position === 'right') {
+                popularCarrousel.current.scrollLeft =
+                    popularCarrousel.current.scrollLeft + 500;
+            }
+        }
+    };
+    const handleSlideTwo = (position: 'left' | 'right') => {
+        if (trendCarrousel.current !== null) {
+            if (position === 'left') {
+                trendCarrousel.current.scrollLeft =
+                    trendCarrousel.current.scrollLeft - 500;
+            }
+            if (position === 'right') {
+                trendCarrousel.current.scrollLeft =
+                    trendCarrousel.current.scrollLeft + 500;
+            }
+        }
+    };
 
     return (
         <>
@@ -88,17 +123,53 @@ export const Main = () => {
                     </Menu>
                 </Stack>
                 <Stack
+                    alignItems="center"
                     as="article"
                     direction={['row']}
-                    maxHeight={['370px']}
+                    maxWidth="99%"
+                    minHeight={['450px']}
                     overflowX={['scroll']}
                     overflowY={['hidden']}
                     pl={['15px']}
+                    ref={popularCarrousel}
+                    scrollBehavior="smooth"
                     spacing={3}
+                    sx={{
+                        '::-webkit-scrollbar': {
+                            display: 'none',
+                        },
+                    }}
                 >
+                    <Button
+                        borderRadius="50%"
+                        colorScheme="red"
+                        height="40px"
+                        onClick={() => handleSlideOne('left')}
+                        position="absolute"
+                        width="40px"
+                        zIndex={100}
+                    >
+                        <ArrowLeftIcon
+                            color="white"
+                            fontSize={20}
+                            zIndex={100}
+                        />
+                    </Button>
                     {moviesByFilter?.map((movie: Movie) => (
                         <Card data={movie} key={movie.id} />
                     ))}
+                    <Button
+                        borderRadius="50%"
+                        colorScheme="red"
+                        height="40px"
+                        onClick={() => handleSlideOne('right')}
+                        position="absolute"
+                        right={['5', '10']}
+                        width="40px"
+                        zIndex={100}
+                    >
+                        <ArrowRightIcon color="white" fontSize={20} />
+                    </Button>
                 </Stack>
             </Box>
             <Stack as="section" direction={['row']} m={['35px 0 -15px 15px']}>
@@ -136,17 +207,48 @@ export const Main = () => {
                 </Menu>
             </Stack>
             <Stack
+                alignItems="center"
                 as="article"
                 direction={['row']}
-                maxHeight={['370px']}
+                minHeight={['450px']}
                 overflowX={['scroll']}
                 overflowY={['hidden']}
                 pl={['15px']}
+                ref={trendCarrousel}
+                scrollBehavior="smooth"
                 spacing={3}
+                sx={{
+                    '::-webkit-scrollbar': {
+                        display: 'none',
+                    },
+                }}
             >
+                <Button
+                    borderRadius="50%"
+                    colorScheme="red"
+                    height="40px"
+                    onClick={() => handleSlideTwo('left')}
+                    position="absolute"
+                    width="40px"
+                    zIndex={100}
+                >
+                    <ArrowLeftIcon color="white" fontSize={20} zIndex={100} />
+                </Button>
                 {moviesByTrend?.map((movie: Movie) => (
                     <Card data={movie} key={movie.id} />
                 ))}
+                <Button
+                    borderRadius="50%"
+                    colorScheme="red"
+                    height="40px"
+                    onClick={() => handleSlideTwo('right')}
+                    position="absolute"
+                    right={['5', '10']}
+                    width="40px"
+                    zIndex={100}
+                >
+                    <ArrowRightIcon color="white" fontSize={20} />
+                </Button>
             </Stack>
         </>
     );
